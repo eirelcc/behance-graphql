a2e2fa79db0d7961c7b0332581ef25da4326c2fd
eirelcc <eirelcc@gmail.com>
2018-02-25 23:23:47 +0200

    Add basic server & query

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+
+const resolvers = {
+    Query,
+    Mutation
+};
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'http://localhost:4466/hackernews-graphql-js/dev',
+            secret: 'mysecret123'
+        })
+    })
+});
+
+server.start(() => console.log('Server is running on http://localhost:4000'));
