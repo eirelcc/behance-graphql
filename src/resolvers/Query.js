814a441ea0ce3a26668c834f506dbca83c039a03
eirelcc <eirelcc@gmail.com>
2018-03-08 16:45:29 +0200

    Add project query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,20 +2,23 @@ const qs = require('qs');
 
 const BASE_URL = 'https://api.behance.net/v2';
 
-async function fetchResponseByURL(relativeURL, params) {
+async function fetchByURL(relativeURL, params) {
     const queryParams = qs.stringify({
         ...params,
         client_id: process.env.CLIENT_ID
     });
     const res = await fetch(`${BASE_URL}${relativeURL}?${queryParams}`);
-    return res.json();
+    const json = await res.json();
+    const [data] = json ? Object.values(json) : [];
+
+    return data;
 }
 
-const projects = async (parent, args, ctx, info) => {
-    const json = await fetchResponseByURL(`/projects`, args);
-    return json.projects;
-};
+const projects = (parent, args, ctx, info) => fetchByURL(`/projects`, args);
+
+const project = (parent, args, ctx, info) => fetchByURL(`/projects/${args.id}`);
 
 module.exports = {
-    projects
+    projects,
+    project
 };
