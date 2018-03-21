fea63a6d13f254510304f32c4cd046779df50b17
eirelcc <eirelcc@gmail.com>
2018-03-21 16:10:03 +0200

    add project images

--- src/resolvers/Project.js
+++ src/resolvers/Project.js
@@ -1,7 +1,9 @@
 const { fetchByURL } = require('../utils');
+const { images } = require('./shared');
 
 const comments = project => fetchByURL(`/projects/${project.id}/comments`);
 
 module.exports = {
-    comments
+    comments,
+    images
 };
