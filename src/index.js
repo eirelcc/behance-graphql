481754be7120787f06e61924a1ac4813265b3c83
eirelcc <eirelcc@gmail.com>
2018-03-23 16:15:38 +0200

    add debug for non-prod

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const server = new GraphQLServer({
         db: new Prisma({
             typeDefs: path.join(__dirname, 'generated/prisma.graphql'),
             endpoint: process.env.PRISMA_ENDPOINT,
-            secret: process.env.PRISMA_SECRET
+            secret: process.env.PRISMA_SECRET,
+            debug: process.env.NODE_ENV !== 'production'
         })
     })
 });
